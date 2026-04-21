918fa369dbd070b614a9868554a11930c1ced1ad
Justin Pitera <justinpitera@gmail.com>
2026-04-21 15:34:01 -0400

    comment

--- 45.jump-game-ii.py
+++ 45.jump-game-ii.py
@@ -3,7 +3,7 @@
 #
 # [45] Jump Game II
 #
-
+from typing import List
 # @lc code=start
 class Solution:
     def jump(self, nums: List[int]) -> int:
@@ -12,7 +12,8 @@ class Solution:
         end = 0
         jumps = 0
         
-        for i in range(length - 1):
+        for i in range(length - 1): # skip last index, no need to jump
+            print(i)
             max_reach = max(max_reach, i + nums[i])
             
             if i == end:
@@ -24,3 +25,7 @@ class Solution:
         
 # @lc code=end
 
+sol = Solution()
+
+res = sol.jump([2,3,1,1,4])
+print(res)
